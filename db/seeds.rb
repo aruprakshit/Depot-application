b9c74f9f4672ce7550a830b6a46dfee7256275a8
Arup Rakshit <aruprakshit@rocketmail.com>
2014-04-27 19:39:21 +0530

    iteration a2,b1,b2 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Product.delete_all
+
+Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+                description:
+                %{<p>
+                     Ruby is the fastest growing and most exciting dynamic language
+                     out there. If you need to get working programs delivered fast,
+                     you should add Ruby to your toolbox.
+                </p>},
+               image_url: 'Programming_Ruby_19_20.png',
+               price: 49.95)
+
+Product.create!(title: 'The Definitive Guide to MySQL5',
+                description:
+                %{<p>
+                    Before we can answer the central question of this chapter, namely, What is MySQL? you and I must
+                    find a common language. Therefore, this section presents a rudimentary database glossary, without going
+                    into great detail.
+                </p>},
+               image_url: 'The_Definitive_Guide_to_MySQL5.png',
+               price: 89.95)
+
+
